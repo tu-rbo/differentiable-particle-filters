e885fce3ca6b85be6ca29b2e7ec7d0332257fdfa
Rico Jonschkowski <rico.j@gmx.de>
2018-05-24 09:48:41 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ mkdir log
 mkdir plots
 echo 'Downloading data (2.5GB, this might take a bit) .. '
 echo
-wget -N 'https://tubcloud.tu-berlin.de/s/LbTYkGbD8ugZoEj/download'
+wget -N 'https://tubcloud.tu-berlin.de/s/0fU32cq0ppqdGXe/download'
 echo 'Unpacking data .. '
 echo
 unzip download
