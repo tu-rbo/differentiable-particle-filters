bf5eb3267831045337b00c0d54547c8b35aa3988
Divyam Rastogi <d.rastogi@tu-berlin.de>
2019-02-01 14:08:34 +0100

    changed pipeline to keras

--- methods/deepvo_lstm.py
+++ methods/deepvo_lstm.py
@@ -17,7 +17,7 @@ class DeepVOLSTM():
         self.init_with_true_state = init_with_true_state
         self.model = model
 
-
+        self.image_input = tf.keras.Input(shape=(None, 384, 1280, 3), name='input_layer')
         # build models
         # self.encoder = snt.Module(name='FlowNetS', build=self.custom_build)
 
@@ -82,22 +82,25 @@ class DeepVOLSTM():
         # stacked_dataset = stacked_dataset.shuffle(buffer_size=10000)
         # dataset = dataset.shuffle(buffer_size=10000)
         dataset = dataset.batch(10)
-        # dataset = dataset.batch(2)
+        dataset = dataset.batch(2)
         iterator = dataset.make_initializable_iterator()
 
         next_image, next_state = iterator.get_next()
+        self.image_input = tf.keras.Input(tensor=next_image)
         # means, stds, state_step_sizes, state_mins, state_maxs = compute_staticstics(data['train'])   #Needs to be changed
 
         ###################### Connecting model and computing loss ##########################
-        self.connect_modules(next_image)
-        sq_dist = compute_sq_distance(self.pred_states, next_state)
-        losses = {'mse': tf.reduce_mean(sq_dist),
-                  'mse_last': tf.reduce_mean(sq_dist[:, -1])}
+        # sq_dist = tf.keras.losses.mean_squared_error(self.pred_states, next_state)
+        # losses = {'mse': tf.reduce_mean(sq_dist),
+        #           'mse_last': tf.reduce_mean(sq_dist[:, -1])}
 
         optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
-        gradients = optimizer.compute_gradients(losses['mse'])
+        # gradients = optimizer.compute_gradients(losses['mse'])
         # clipped_gradients = [(tf.clip_by_value(grad, -1., 1.), var) for grad, var in gradients]
-        train_op = optimizer.apply_gradients(gradients)
+        # train_op = optimizer.apply_gradients(gradients)
+
+        self.model = self.connect_modules()
+        self.model.compile(optimizer=optimizer, loss=tf.keras.losses.mean_squared_error, target_tensors=[next_state], metrics=['mae'])
 
         init = tf.global_variables_initializer()
         sess.run(init)
@@ -128,93 +131,120 @@ class DeepVOLSTM():
             loss_lists = {lk: [] for lk in loss_keys}
             batches_length = 0
             while True:
-                try:
-                    a = sess.run(next_image)
-                    # b = sess.run(next_state)
-                    s_losses, _ = sess.run([losses, train_op])
-                    for lk in loss_keys:
-                        loss_lists[lk].append(s_losses[lk])
-                        batches_length += 1
-                except tf.errors.OutOfRangeError:
-                    break
-            log[lk]['mean'].append(np.mean(loss_lists[lk]))
-            log[lk]['se'].append(np.std(loss_lists[lk], ddof=1) / np.sqrt(batches_length))
-
-            txt = ''
-            for lk in loss_keys:
-                txt += '{}: '.format(lk)
-                for dk in data_keys:
-                    txt += '{:.2f}+-{:.2f}/'.format(log[dk][lk]['mean'][-1], log[dk][lk]['se'][-1])
-                txt = txt[:-1] + ' -- '
-            print(txt)
-        # i = 0
-        # while i < num_epochs and i - best_epoch < patience:
-        #     # training
-        #     loss_lists = dict()
-        #     for dk in data_keys:
-        #         loss_lists = {lk: [] for lk in loss_keys}
-        #         for e in range(epoch_lengths[dk]):
-        #             batch = next(batch_iterators[dk])
-        #             if dk == 'train':
-        #                 s_losses, _ = sess.run([losses, train_op], {**{self.placeholders[key]: batch[key] for key in 'osa'},
-        #                                                         **{self.placeholders['keep_prob']: dropout_keep_ratio}})
-        #             else:
-        #                 s_losses = sess.run(losses, {**{self.placeholders[key]: batch[key] for key in 'osa'},
-        #                                                     **{self.placeholders['keep_prob']: 1.0}})
+                # try:
+                # a = sess.run(next_image)
+                # b = sess.run(next_state)
+                self.model.fit(steps_per_epoch=1, epochs=5, verbose=2)
+        #           s_losses, _ = sess.run([losses, train_op])
         #             for lk in loss_keys:
         #                 loss_lists[lk].append(s_losses[lk])
-        #         # after each epoch, compute and log statistics
-        #         for lk in loss_keys:
-        #             log[dk][lk]['mean'].append(np.mean(loss_lists[lk]))
-        #             log[dk][lk]['se'].append(np.std(loss_lists[lk], ddof=1) / np.sqrt(epoch_lengths[dk]))
+        #                 batches_length += 1
+        #         except tf.errors.OutOfRangeError:
+        #             break
+        #     log[lk]['mean'].append(np.mean(loss_lists[lk]))
+        #     log[lk]['se'].append(np.std(loss_lists[lk], ddof=1) / np.sqrt(batches_length))
         #
-        #     # check whether the current model is better than all previous models
-        #     if 'val' in data_keys:
-        #         if log['val']['mse_last']['mean'][-1] < best_val_loss:
-        #             best_val_loss = log['val']['mse_last']['mean'][-1]
-        #             best_epoch = i
-        #             # save current model
-        #             saver.save(sess, save_path)
-        #             txt = 'epoch {:>3} >> '.format(i)
-        #         else:
-        #             txt = 'epoch {:>3} == '.format(i)
-        #     else:
-        #         best_epoch = i
-        #         saver.save(sess, save_path)
-        #         txt = 'epoch {:>3} >> '.format(i)
-        #
-        #     # after going through all data sets, do a print out of the current result
+        #     txt = ''
         #     for lk in loss_keys:
         #         txt += '{}: '.format(lk)
         #         for dk in data_keys:
         #             txt += '{:.2f}+-{:.2f}/'.format(log[dk][lk]['mean'][-1], log[dk][lk]['se'][-1])
         #         txt = txt[:-1] + ' -- '
         #     print(txt)
+        # # i = 0
+        # # while i < num_epochs and i - best_epoch < patience:
+        # #     # training
+        # #     loss_lists = dict()
+        # #     for dk in data_keys:
+        # #         loss_lists = {lk: [] for lk in loss_keys}
+        # #         for e in range(epoch_lengths[dk]):
+        # #             batch = next(batch_iterators[dk])
+        # #             if dk == 'train':
+        # #                 s_losses, _ = sess.run([losses, train_op], {**{self.placeholders[key]: batch[key] for key in 'osa'},
+        # #                                                         **{self.placeholders['keep_prob']: dropout_keep_ratio}})
+        # #             else:
+        # #                 s_losses = sess.run(losses, {**{self.placeholders[key]: batch[key] for key in 'osa'},
+        # #                                                     **{self.placeholders['keep_prob']: 1.0}})
+        # #             for lk in loss_keys:
+        # #                 loss_lists[lk].append(s_losses[lk])
+        # #         # after each epoch, compute and log statistics
+        # #         for lk in loss_keys:
+        # #             log[dk][lk]['mean'].append(np.mean(loss_lists[lk]))
+        # #             log[dk][lk]['se'].append(np.std(loss_lists[lk], ddof=1) / np.sqrt(epoch_lengths[dk]))
+        # #
+        # #     # check whether the current model is better than all previous models
+        # #     if 'val' in data_keys:
+        # #         if log['val']['mse_last']['mean'][-1] < best_val_loss:
+        # #             best_val_loss = log['val']['mse_last']['mean'][-1]
+        # #             best_epoch = i
+        # #             # save current model
+        # #             saver.save(sess, save_path)
+        # #             txt = 'epoch {:>3} >> '.format(i)
+        # #         else:
+        # #             txt = 'epoch {:>3} == '.format(i)
+        # #     else:
+        # #         best_epoch = i
+        # #         saver.save(sess, save_path)
+        # #         txt = 'epoch {:>3} >> '.format(i)
+        # #
+        # #     # after going through all data sets, do a print out of the current result
+        # #     for lk in loss_keys:
+        # #         txt += '{}: '.format(lk)
+        # #         for dk in data_keys:
+        # #             txt += '{:.2f}+-{:.2f}/'.format(log[dk][lk]['mean'][-1], log[dk][lk]['se'][-1])
+        # #         txt = txt[:-1] + ' -- '
+        # #     print(txt)
+        # #
+        # #     i += 1
         #
-        #     i += 1
-
-        saver.restore(sess, save_path)
+        # saver.restore(sess, save_path)
 
         return log
 
 
-    def connect_modules(self, inputs):
+    def connect_modules(self):
 
-        encoder_output = self.custom_build(inputs)
-
-        rnn_layers = [tf.nn.rnn_cell.LSTMCell(size) for size in [1000, 1000]]
-
-        # create a RNN cell composed sequentially of a number of RNNCells
-        lstm_core = tf.nn.rnn_cell.MultiRNNCell(rnn_layers)
-
-        output_sequence, final_state = tf.nn.dynamic_rnn(
-            cell=lstm_core,
-            inputs=encoder_output,
-            time_major=False,
-             dtype=tf.float32)
-
-        self.pred_states = tf.layers.dense(output_sequence, units=3)
+        # encoder_output = self.custom_build(inputs)
+        #
+        # conv_flat = tf.keras.layers.Flatten()(encoder_output)
+        #
+        # rnn_layers = [tf.nn.rnn_cell.LSTMCell(size) for size in [1000, 1000]]
+        #
+        # # create a RNN cell composed sequentially of a number of RNNCells
+        # lstm_core = tf.nn.rnn_cell.MultiRNNCell(rnn_layers)
+        #
+        # output_sequence, final_state = tf.nn.dynamic_rnn(
+        #     cell=lstm_core,
+        #     inputs=encoder_output,
+        #     time_major=False,
+        #      dtype=tf.float32)
+        #
+        # self.pred_states = tf.layers.dense(output_sequence, units=3)
         # self.pred_states = self.pred_states * stds['s'] + means['s']
+
+        self.model = tf.keras.Sequential()
+        self.model.add(tf.keras.layers.Conv2D(64, kernel_size=(7, 7), strides=(2, 2), padding='valid'))
+        self.model.add(tf.keras.layers.Conv2D(128, kernel_size=(5, 5), strides=(2, 2), padding='valid'))
+        self.model.add(tf.keras.layers.Conv2D(256, kernel_size=(5, 5), strides=(2, 2), padding='valid'))
+        self.model.add(tf.keras.layers.Conv2D(256, kernel_size=(3, 3), strides=(1, 1), padding='valid'))
+        self.model.add(tf.keras.layers.Conv2D(512, kernel_size=(3, 3), strides=(2, 2), padding='valid'))
+        self.model.add(tf.keras.layers.Conv2D(512, kernel_size=(3, 3), strides=(1, 1), padding='valid'))
+        self.model.add(tf.keras.layers.Conv2D(512, kernel_size=(3, 3), strides=(2, 2), padding='valid'))
+        self.model.add(tf.keras.layers.Conv2D(512, kernel_size=(3, 3), strides=(1, 1), padding='valid'))
+        self.model.add(tf.keras.layers.Conv2D(1024, kernel_size=(3, 3), strides=(2, 2), padding='valid'))
+        self.model.add(tf.keras.layers.Flatten())
+
+        time_distribute = tf.keras.layers.TimeDistributed(tf.keras.layers.Lambda(lambda x: self.model(x)))(self.image_input)
+
+        lstm1 = tf.keras.layers.LSTM(1000, return_sequences=True)(time_distribute)
+        lstm2 = tf.keras.layers.LSTM(1000, return_sequences=True)(lstm1)
+
+        self.pred_states = tf.keras.layers.Dense(3, activation='relu')(lstm2)
+
+        model = tf.keras.Model(inputs=[self.image_input], outputs=[self.pred_states])
+
+        return model
+
 
     def predict(self, sess, batch, **unused_kwargs):
         return sess.run(self.pred_states, batch)
